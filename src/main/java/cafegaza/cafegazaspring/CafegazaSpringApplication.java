ead5c50acff403368ebe25ab51deff1e5b8ab7f9
hyein0229 <l1253@naver.com>
2023-08-28 15:17:02 +0900

    first setting

---
+++ src/main/java/cafegaza/cafegazaspring/CafegazaSpringApplication.java
@@ -0,0 +1,13 @@
+package cafegaza.cafegazaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafegazaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafegazaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cafegaza/cafegazaspring/CafegazaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package cafegaza.cafegazaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafegazaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
