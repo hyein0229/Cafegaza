95137f494cf9af907ad81be1a1c84d3787cbb08f
hyein0229 <l1253@naver.com>
2023-11-05 18:17:08 +0900

    feat(#23) : Bookmark 추가를 위한 Controller 메소드 추가
    Controller 에 addBookMark 메소드 생성
    북마크를 누른 대상인 카페의 아이디를 URI 의 파라미터로부터 가져오도록 하고
    BookmarkService 의 addBookMark 메소드를 호출하여 생성
    이 때, 서비스 호출 시 dto 를 파라미터로 주는 것이 아니라 id만 받도록 수정

--- src/main/java/cafegaza/cafegazaspring/controller/BookmarkController.java
+++ src/main/java/cafegaza/cafegazaspring/controller/BookmarkController.java
@@ -1,4 +1,34 @@
 package cafegaza.cafegazaspring.controller;
 
+import cafegaza.cafegazaspring.dto.CafeDto;
+import cafegaza.cafegazaspring.service.BookmarkService;
+import cafegaza.cafegazaspring.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailParseException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
 public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    /**
+     * 북마크 추가하기
+     */
+    @PostMapping("/bookmark/{cafeId}")
+    public ResponseEntity addBookMark(@SessionAttribute(name = "sessionId", required = false) Long memberId, @PathVariable("cafeId") Long cafeId) {
+        if(memberId == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); // 로그인이 되어있지 않아 권한 없음
+        }
+
+        bookmarkService.addBookmark(cafeId, memberId);
+        return new ResponseEntity<>(HttpStatus.OK); // 성공적으로 요청 처리됨
+    }
+
 }
