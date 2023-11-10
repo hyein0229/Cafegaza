e9f54bac7376bca41548db086ca8eb027fb05122
hyein0229 <l1253@naver.com>
2023-11-10 23:36:15 +0900

    feat : 카페 엔티티를 찾을 때 memberId 파라미터 추가
    카페를 찾고 dto 반환 시
    사용자에 따른 카페의 북마크 여부 판단을 위한 memberId 파라미터 추가

--- src/main/java/cafegaza/cafegazaspring/controller/SearchQuery.java
+++ src/main/java/cafegaza/cafegazaspring/controller/SearchQuery.java
@@ -1,5 +1,6 @@
 package cafegaza.cafegazaspring.controller;
 
+import cafegaza.cafegazaspring.domain.Member;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +9,7 @@ import lombok.Setter;
 @Setter
 public class SearchQuery {
     private String keyword; // 검색 키워드
-    private String menuOption; // 메뉴 이름 검색
+    private String menuOption = ""; // 메뉴 이름 검색
     private int maxPrice = 0; // 가격 옵션
     private int startHour = 0; // 영업 시간1 (범위 시작)
     private int endHour = 0; // 영업 시간2 (범위 끝)
