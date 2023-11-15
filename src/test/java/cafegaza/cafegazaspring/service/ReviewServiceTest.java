dea055ffcf99b626e90ce14bee4714512b63af77
hyein0229 <l1253@naver.com>
2023-11-15 21:38:45 +0900

    style(#29) : ReviewForm 의 package 위치 변경으로 인한 수정

--- src/test/java/cafegaza/cafegazaspring/service/ReviewServiceTest.java
+++ src/test/java/cafegaza/cafegazaspring/service/ReviewServiceTest.java
@@ -1,9 +1,8 @@
 package cafegaza.cafegazaspring.service;
 
-import cafegaza.cafegazaspring.controller.ReviewForm;
+import cafegaza.cafegazaspring.dto.ReviewForm;
 import cafegaza.cafegazaspring.domain.Review;
 import cafegaza.cafegazaspring.repository.ReviewRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,12 +30,14 @@ public class ReviewServiceTest {
     public void createReviewTest() throws Exception {
 
         // 리뷰 폼 작성
-        Long cafeId = Long.valueOf(1L); // 리뷰 대상 카페 id 1번 -> 어반플랜트 합정
+        Long cafeId = Long.valueOf(2L); // 리뷰 대상 카페 id
         ReviewForm reviewForm = new ReviewForm();
         reviewForm.setContent("wgwgwwegwefsgdsdsssssssssssssssssssssssssssssssssss" +
                 "wegggggggggg");
-        reviewForm.setRate(3);
+        reviewForm.setRate(5);
         reviewForm.setImageFiles(new ArrayList<>());
+
+        reviewService.create(cafeId, 9999L, reviewForm);
 
     }
     /*
@@ -47,20 +48,15 @@ public class ReviewServiceTest {
     public void updateReviewTest() throws Exception {
 
         // 기존 리뷰를 찾아 수정
-        Review findReview = reviewService.findOne(52L); // id 2번 리뷰 수정
+        Review findReview = reviewService.findOne(49952L); // id 2번 리뷰 수정
         ReviewForm reviewForm = new ReviewForm();
         reviewForm.setId(findReview.getReviewId());
         reviewForm.setContent("리뷰 수정 테스트입니다.");
-        reviewForm.setRate(findReview.getRate());
+        reviewForm.setRate(3);
         reviewForm.setImageFiles(new ArrayList<>());
 
         // 리뷰 수정
-        reviewService.update(52L, reviewForm);
-
-        // then
-        Review updatedReview = reviewRepository.findById(52L).get();
-        Assertions.assertEquals("리뷰 수정 테스트입니다.", updatedReview.getContent());
-        Assertions.assertEquals("캡처.PNG", updatedReview.getReviewImages().get(0).getFileOrigName());
+        reviewService.update(49952L, reviewForm);
 
     }
 
@@ -70,7 +66,7 @@ public class ReviewServiceTest {
     @Test
     @Rollback(value = false)
     public void deleteReviewTest() {
-        reviewService.delete(1052L);
+        reviewService.delete(49952L);
 
     }
 
