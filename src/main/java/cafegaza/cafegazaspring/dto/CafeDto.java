48bfd9a26b2dc2d9857584e428492c0a717f115c
hyein0229 <l1253@naver.com>
2023-11-15 21:19:17 +0900

    style(#29) : 영업 여부 체크 메소드를 서비스에서 dto 로 이동
    service 에서 영업 여부를 체크하여 set 하지 않고
    dto 에서 entity -> dto 변환 시 영업 여부를 체크할 수 있도록 함.

--- src/main/java/cafegaza/cafegazaspring/dto/CafeDto.java
+++ src/main/java/cafegaza/cafegazaspring/dto/CafeDto.java
@@ -1,13 +1,18 @@
 package cafegaza.cafegazaspring.dto;
 
 import cafegaza.cafegazaspring.domain.Cafe;
+import cafegaza.cafegazaspring.domain.OpenHour;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Optional;
 
 @Builder
 @Data
@@ -65,6 +70,7 @@ public class CafeDto {
                 .rate(cafe.getRate())
                 .reviewCount(cafe.getReviewCount())
                 .menus(cafe.getMenus().stream().map(menu -> MenuDto.toDto(menu)).toList())
+                .isOpen(checkOpen(cafe))
                 .build();
     }
 
@@ -78,6 +84,29 @@ public class CafeDto {
     }
     public void setIsBookmark (boolean isBookmark){
         this.isBookmark = isBookmark;
+    }
+
+    public static boolean checkOpen(Cafe cafe) {
+        LocalTime now = LocalTime.now();
+        int currentTime = now.getHour() * 60 + now.getMinute();
+        String today = getToday();
+        Optional<OpenHour> todayOpenHour = cafe.getOpenHourList().stream().filter(openHour -> openHour.getDay().equals(today)).findFirst();
+        if (!todayOpenHour.isEmpty()) {
+            int startTime = todayOpenHour.get().getStartTime();
+            int endTime = todayOpenHour.get().getEndTime();
+            if (startTime < currentTime && endTime > currentTime) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private static String getToday() {
+        List<String> list = Arrays.asList("일", "월", "화", "수", "목", "금", "토", "일");
+        Calendar cal = Calendar.getInstance();
+        String dayOfWeek = list.get(cal.get(Calendar.DAY_OF_WEEK) - 1); // 현재 요일 구하기, 1~7 -> 일, 월~금, 토
+        return dayOfWeek;
     }
 
 
